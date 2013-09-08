0e621a096854eb8bb3a3318d635b7665e1a4a57f
David Moreau <david.moreau@thalesaleniaspace.com>
2013-09-08 12:04:19 +0200

    Rename_photo fix

--- rename_photos.sh
+++ rename_photos.sh
@@ -23,36 +23,40 @@ while getopts "do:" opt; do
   esac
 done
 
-if [ -d "$OUTPUT_DIR" ]; then
-	echo "Output dir ($OUTPUT_DIR) allready exists!"
-	exit 1;
-fi
+#if [ -d "$OUTPUT_DIR" ]; then
+#	echo "Output dir ($OUTPUT_DIR) allready exists!"
+#	exit 1;
+#fi
 
 # Nombre de fichier à taiter
-NB_FILES=$(ls -l *.jpg *.JPG 2>/dev/null |wc -l)
+NB_FILES=$(ls -l *.jpg *.JPG *.jpeg 2>/dev/null |wc -l)
 
 # create output dir
 mkdir -p "$OUTPUT_DIR"
 
-find  -maxdepth 1 -name "*.jpg" -o -name "*.JPG" | while read i
+find  -maxdepth 1 -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" | while read i
 do
-	NAME=$(exif -m "$i" | perl -lne 'print "$1-$2-$3_$4.$5.$6" if /Date and Time \(Original\)\t(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/')
+	NAME=$(exif -m "$i" 2>/dev/null| perl -lne 'print "$1-$2-$3_$4.$5.$6" if /Date and Time \(Original\)\t(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/')
 	if [ $? -eq 1 -o -z "$NAME" ]; then
-		echo "exif failed, try exiv2..."
-		NAME=$(exiv2 "$i" | perl -lne 'print "$1-$2-$3_$4.$5.$6" if /Image timestamp : (\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/')
+		#echo "exif failed, try exiv2..."
+		NAME=$(exiv2 "$i" 2>/dev/null| perl -lne 'print "$1-$2-$3_$4.$5.$6" if /Image timestamp : (\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/')
 		if [ $? -eq 1 -o -z "$NAME" ]; then
 			echo "exiv2 failed, cannot rename $i"
-			NAME=$i
+			NAME=$(basename $i)
 		fi
 	fi
 
-	FILESIZE=$(du "$i" | cut -f1)
-	if [ $FILESIZE -gt $SIZE_MAX ]; then 
-		[ $DRYRUN -eq 0 ] && convert "$i" -resize x$RESIZE "$OUTPUT_DIR/$NAME.jpg"
-		echo "Resize $i ==> $NAME.jpg"
+	if [ -f "$OUTPUT_DIR/$NAME.jpg" ]; then
+		echo "$OUTPUT_DIR/$NAME.jpg allready exits ($i)"
 	else
-		[ $DRYRUN -eq 0 ] && cp "$i" "$OUTPUT_DIR/$NAME.jpg"
-		echo "Copy $i ==> $NAME.jpg"
+		FILESIZE=$(du "$i" | cut -f1)
+		if [ $FILESIZE -gt $SIZE_MAX ]; then 
+			[ $DRYRUN -eq 0 ] && convert "$i" -resize x$RESIZE "$OUTPUT_DIR/$NAME.jpg"
+			echo "Resize $i ==> $NAME.jpg"
+		else
+			[ $DRYRUN -eq 0 ] && cp "$i" "$OUTPUT_DIR/$NAME.jpg"
+			echo "Copy $i ==> $NAME.jpg"
+		fi
 	fi
 done
 
