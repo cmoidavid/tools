e45cc3ece7cc29a20211f5aaac8f2fa3362f12d3
David Moreau <david.moreau@thalesaleniaspace.com>
2013-09-08 14:22:15 +0200

    rename_photos.sh update

--- rename_photos.sh
+++ rename_photos.sh
@@ -3,16 +3,14 @@
 # Script which renames photos
 
 OUTPUT_DIR=$OUTPUT_DIR
-DRYRUN=0
 SIZE_MAX=1000
-RESIZE=1920
+RESIZE=1024
 
 
-while getopts "do:" opt; do
+while getopts "o:r:" opt; do
   case $opt in
-    d)
-      echo "Dry run mode" >&2
-      DRYRUN=1
+    r)
+      RESIZE=$OPTARG
       ;;
     o)
       OUTPUT_DIR=$OPTARG
@@ -23,48 +21,68 @@ while getopts "do:" opt; do
   esac
 done
 
-#if [ -d "$OUTPUT_DIR" ]; then
-#	echo "Output dir ($OUTPUT_DIR) allready exists!"
-#	exit 1;
-#fi
 
 # Nombre de fichier à taiter
 NB_FILES=$(ls -l *.jpg *.JPG *.jpeg 2>/dev/null |wc -l)
 
+# Summary
+echo "OUTPUT_DIR=$OUTPUT_DIR"
+echo "RESIZE=$RESIZE"
+echo "NB_FILES=$NB_FILES"
+
+if [ -d "$OUTPUT_DIR" ]; then
+	echo "Output dir ($OUTPUT_DIR) allready exists, remove? (y/N)"
+	read c
+	if [ "$c" = y ]; then
+		rm -rf $OUTPUT_DIR
+	else
+		exit 0
+	fi
+fi
+
 # create output dir
-mkdir -p "$OUTPUT_DIR"
+mkdir  "$OUTPUT_DIR"
 
 find  -maxdepth 1 -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" | while read i
 do
-	NAME=$(exif -m "$i" 2>/dev/null| perl -lne 'print "$1-$2-$3_$4.$5.$6" if /Date and Time \(Original\)\t(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/')
-	if [ $? -eq 1 -o -z "$NAME" ]; then
-		#echo "exif failed, try exiv2..."
-		NAME=$(exiv2 "$i" 2>/dev/null| perl -lne 'print "$1-$2-$3_$4.$5.$6" if /Image timestamp : (\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/')
-		if [ $? -eq 1 -o -z "$NAME" ]; then
-			echo "exiv2 failed, cannot rename $i"
-			NAME=$(basename $i)
-		fi
-	fi
-
-	if [ -f "$OUTPUT_DIR/$NAME.jpg" ]; then
-		echo "$OUTPUT_DIR/$NAME.jpg allready exits ($i)"
+	FILESIZE=$(du "$i" | cut -f1)
+	SIZE=$(du -h "$i" | cut -f1)
+	if [ $FILESIZE -gt $SIZE_MAX ]; then 
+		cp "$i" "$OUTPUT_DIR/$i"
+		SIZE=$(du -h "$i" | cut -f1)
+		SIZE2=$(du -h "$OUTPUT_DIR/$i" | cut -f1)
+		echo "Copy $i ($SIZE) into $OUTPUT_DIR/$i ($SIZE2)"
 	else
-		FILESIZE=$(du "$i" | cut -f1)
-		if [ $FILESIZE -gt $SIZE_MAX ]; then 
-			[ $DRYRUN -eq 0 ] && convert "$i" -resize x$RESIZE "$OUTPUT_DIR/$NAME.jpg"
-			echo "Resize $i ==> $NAME.jpg"
-		else
-			[ $DRYRUN -eq 0 ] && cp "$i" "$OUTPUT_DIR/$NAME.jpg"
-			echo "Copy $i ==> $NAME.jpg"
-		fi
+		convert "$i" -resize "${RESIZE}>x${RESIZE}>" "$OUTPUT_DIR/$i"
+		SIZE=$(du -h "$i" | cut -f1)
+		SIZE2=$(du -h "$OUTPUT_DIR/$i" | cut -f1)
+		echo "Resize $i ($SIZE) into $OUTPUT_DIR/$i ($SIZE2)"
 	fi
 done
 
-NB_FILES_RENAMED=$(cd "$OUTPUT_DIR" && ls -l *.jpg *.JPG 2>/dev/null |wc -l)
+# rename
+cd $OUTPUT_DIR
+find  -maxdepth 1 -name "*.jpg" | while read i
+do
+	NAME=$(exiftool -TAG -CreateDate "$i" -d "%Y-%m-%d.%Hh%Mmin%Ss" -s -s -s)
+	if [ $? -eq 1 -o -z "$NAME" ]; then
+		echo "Retrieving creation date failed"
+		continue
+	fi
+	if [ -f "$NAME.jpg" ]; then
+		echo "$NAME.jpg allready exits, don't rename $i"
+		continue
+	fi
+	mv "$i" "$NAME.jpg"
+	echo "Rename $i into $NAME.jpg"
+done
+
+
+NB_FILES_RENAMED=$(ls -l *.jpg *.JPG 2>/dev/null |wc -l)
 
 
 # stat
-echo "Output directory: $OUTPUT_DIR"
+echo ""
 echo "$NB_FILES file(s) before resizing"
 echo "$NB_FILES_RENAMED file(s) after resizing"
 
